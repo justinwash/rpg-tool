9263a84c53640d3048db83a4480b3e76ffb1ace6
justin wash <justin.wash@pm.me>
2021-02-24 17:46:41 -0600

    wasm logging

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,18 +5,24 @@ export const App = () => {
   const [wasm, setWasm] = useState<any>(null);
 
   useEffect(() => {
-    import('wasm').then((m) => setWasm(m));
+    import('wasm').then((m) => {
+      setWasm(m);
+    });
   }, []);
 
-  return wasm ? (
-    <div className='App'>
-      add_two_ints(2, 2) = {wasm.add_two_ints(2, 2)}
-      <br />
-      fib(27) = {wasm.fib(27)}
-    </div>
-  ) : (
-    <Spinner />
-  );
+  if (wasm) {
+    return (
+      <div className='App'>
+        add_two_ints(2, 2) = {wasm.add_two_ints(2, 2)}
+        <br />
+        fib(27) = {wasm.fib(27)}
+        <div id='drag-container'>
+          <img src='./logo192.png' alt='logo' />
+          {wasm.main()}
+        </div>
+      </div>
+    );
+  } else return <Spinner />;
 };
 
 export default App;
--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -4,10 +4,31 @@ use wasm_bindgen::prelude::*;
 pub fn add_two_ints(a: u32, b: u32) -> u32 {
    a + b
 }
+
 #[wasm_bindgen]
 pub fn fib(n: u32) -> u32 {
    if n == 0 || n == 1 {
       return n;
    }
    fib(n - 1) + fib(n - 2)
+}
+
+#[wasm_bindgen]
+pub fn console_log(message: &str) {
+   let array = js_sys::Array::new();
+   array.push(&message.into());
+   web_sys::console::log(&array);
+}
+
+#[wasm_bindgen]
+pub fn main() {
+   use dragula::*;
+
+   let doc = web_sys::window().unwrap().document().unwrap();
+   console_log(&doc.title());
+   //let element = doc.get_element_by_id("drag-container").unwrap();
+
+   // let drake = dragula(&[element]);
+
+   console_log("Hello from wasm!");
 }
