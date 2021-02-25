b8cf52379f349b882c59739b09f7ed4fb534631e
justin wash <justin.wash@pm.me>
2021-02-25 13:05:49 -0600

    draw image to canvas experiment

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -1,94 +1,67 @@
 use wasm_bindgen::prelude::*;
-use std::cell::Cell;
-use std::rc::Rc;
 use wasm_bindgen::JsCast;
 
 #[wasm_bindgen]
-pub fn console_log(message: &str) {
-   let array = js_sys::Array::new();
-   array.push(&message.into());
-   web_sys::console::log(&array);
+pub fn console_log(message: String) {
+  let array = js_sys::Array::new();
+  array.push(&message.into());
+  web_sys::console::log(&array);
 }
 
 #[wasm_bindgen(start)]
 pub fn greeting() {
-   console_log("Hello from wasm!");
+  console_log(format!("Hello from wasm!"));
 }
 
 #[wasm_bindgen]
 pub fn resize_canvas(width: u32, height: u32) -> Result<(), JsValue> {
-    let document = web_sys::window().unwrap().document().unwrap();
-    let canvas = document.get_element_by_id("canvas");
-    match canvas {
-        Some(x) => {
-            x.dyn_ref::<web_sys::HtmlCanvasElement>()
-            .unwrap()
-            .set_width(width);
-            x.dyn_ref::<web_sys::HtmlCanvasElement>()
-            .unwrap()
-            .set_height(height);
-            return Ok(())
-        },
-        None => { return Ok(() )}
+  let document = web_sys::window().unwrap().document().unwrap();
+  let canvas = document.get_element_by_id("canvas");
+  match canvas {
+    Some(x) => {
+      x.dyn_ref::<web_sys::HtmlCanvasElement>()
+        .unwrap()
+        .set_width(width);
+      x.dyn_ref::<web_sys::HtmlCanvasElement>()
+        .unwrap()
+        .set_height(height);
+      return Ok(());
     }
+    None => return Ok(()),
+  }
 }
 
 #[wasm_bindgen]
 pub fn create_canvas() -> Result<(), JsValue> {
-    let document = web_sys::window().unwrap().document().unwrap();
-    let canvas = document
-        .create_element("canvas")?
-        .dyn_into::<web_sys::HtmlCanvasElement>()?;
-    document.body().unwrap().append_child(&canvas)?;
-    canvas.set_width(100);
-    canvas.set_height(100);
-    canvas.style()
-    .set_property("background", "lightpink")?;
-    canvas.set_id("canvas");
-    let context = canvas
-        .get_context("2d")?
-        .unwrap()
-        .dyn_into::<web_sys::CanvasRenderingContext2d>()?;
-    let context = Rc::new(context);
-    let pressed = Rc::new(Cell::new(false));
-    {
-        let context = context.clone();
-        let pressed = pressed.clone();
-        let closure = Closure::wrap(Box::new(move |event: web_sys::MouseEvent| {
-            context.begin_path();
-            context.move_to(event.offset_x() as f64, event.offset_y() as f64);
-            pressed.set(true);
-        }) as Box<dyn FnMut(_)>);
-        canvas.add_event_listener_with_callback("mousedown", closure.as_ref().unchecked_ref())?;
-        closure.forget();
-    }
-    {
-        let context = context.clone();
-        let pressed = pressed.clone();
-        let closure = Closure::wrap(Box::new(move |event: web_sys::MouseEvent| {
-            if pressed.get() {
-                context.line_to(event.offset_x() as f64, event.offset_y() as f64);
-                context.stroke();
-                context.begin_path();
-                context.move_to(event.offset_x() as f64, event.offset_y() as f64);
-            }
-        }) as Box<dyn FnMut(_)>);
-        canvas.add_event_listener_with_callback("mousemove", closure.as_ref().unchecked_ref())?;
-        closure.forget();
-    }
-    {
-        let context = context.clone();
-        let pressed = pressed.clone();
-        let closure = Closure::wrap(Box::new(move |event: web_sys::MouseEvent| {
-            pressed.set(false);
-            context.line_to(event.offset_x() as f64, event.offset_y() as f64);
-            context.stroke();
-        }) as Box<dyn FnMut(_)>);
-        canvas.add_event_listener_with_callback("mouseup", closure.as_ref().unchecked_ref())?;
-        closure.forget();
-    }
+  let document = web_sys::window().unwrap().document().unwrap();
+  let canvas = document
+    .create_element("canvas")?
+    .dyn_into::<web_sys::HtmlCanvasElement>()?;
+  document.body().unwrap().append_child(&canvas)?;
+  canvas.set_width(100);
+  canvas.set_height(100);
+  canvas.set_id("canvas");
+  let context = canvas
+    .get_context("2d")?
+    .unwrap()
+    .dyn_into::<web_sys::CanvasRenderingContext2d>()?;
 
-    console_log("canvas create successful");
+  let image = web_sys::HtmlImageElement::new().unwrap();
 
-    Ok(())
-   }
+  {
+    let img = image.clone();
+    let ctx = context.clone();
+    let draw_image = Closure::wrap(Box::new(move || {
+      ctx
+        .draw_image_with_html_image_element_and_dw_and_dh(&img, 0.0, 0.0, 400.0, 800.0)
+        .expect("drawing image failed");
+    }) as Box<dyn Fn()>);
+    image.set_onload(Some(draw_image.as_ref().unchecked_ref()));
+    draw_image.forget();
+    image.set_src("disco.jpeg");
+  }
+
+  console_log(format!("canvas create successful"));
+
+  Ok(())
+}
