1131d294a75c5bf7b426939ba231873450b243ec
justin wash <justin.wash@pm.me>
2021-02-27 13:21:00 -0600

    switch to pixijs, map scrolling on drag

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,18 +1,55 @@
-import React from 'react';
+import React, { createRef, useEffect } from 'react';
 import playArea from './pixi/playArea';
 
+import useWindowDimensions from '../utilities/window';
+
 const App = () => {
-  const canvasRef = React.createRef<HTMLDivElement>();
+  const canvasRef = createRef<HTMLDivElement>();
 
-  React.useEffect(() => {
+  useEffect(() => {
     canvasRef?.current?.appendChild(playArea.view);
+    return () => playArea.stop();
+  }, []);
 
-    return () => {
-      playArea.stop();
-    };
-  }, [canvasRef, playArea]);
+  const { width, height } = useWindowDimensions();
+  const outerSidebarWidth = 80;
+  const innerSidebarWidth = 400;
 
-  return <div ref={canvasRef} />;
+  useEffect(() => {
+    if (playArea && canvasRef.current) {
+      playArea.renderer.resize(width - outerSidebarWidth - innerSidebarWidth, height);
+    } else {
+      console.log('no canvas to resize :(');
+    }
+  }, [playArea, width, height]);
+
+  return (
+    <div style={{ width: '100vw' }} className='App'>
+      <div
+        id='placeholder-outer-sidebar'
+        style={{
+          background: '#444',
+          width: `${outerSidebarWidth}px`,
+          height: '100vh',
+          float: 'left',
+        }}
+      >
+        options, user profilepic, etc etc
+      </div>
+      <div
+        id='placeholder-inner-sidebar'
+        style={{
+          background: '#888',
+          width: `${innerSidebarWidth}px`,
+          height: '100vh',
+          float: 'left',
+        }}
+      >
+        chat and event log and stuff
+      </div>
+      <div style={{ overflow: 'auto' }} ref={canvasRef} />
+    </div>
+  );
 };
 
 export default App;
--- client/src/components/pixi/playArea.ts
+++ client/src/components/pixi/playArea.ts
@@ -3,71 +3,37 @@ import * as PIXI from 'pixi.js-legacy';
 const playArea = new PIXI.Application({ backgroundColor: 0x1099bb });
 document.body.appendChild(playArea.view);
 
-// create a texture from an image path
-const texture = PIXI.Texture.from('/test_map.png');
-
-// Scale mode for pixelation
-texture.baseTexture.scaleMode = PIXI.SCALE_MODES.NEAREST;
-
 createMap();
 
 function createMap() {
-  // create our little bunny friend..
-  const map = new PIXI.Sprite(texture);
+  const texture = PIXI.Texture.from('/test_map.png');
+  const map = new PIXI.Sprite(texture)
 
-  // enable the bunny to be interactive... this will allow it to respond to mouse and touch events
   map.interactive = true;
-
-  // this button mode will mean the hand cursor appears when you roll over the bunny with your mouse
   map.buttonMode = true;
+  map.anchor.set(0.5)
+  map.scale.set(1);
 
-  // center the bunny's anchor point
-  map.anchor.set(0.5);
-
-  // make it a bit bigger, so it's easier to grab
-  map.scale.set(3);
-
-  // setup events for mouse + touch using
-  // the pointer events
   map
     .on('pointerdown', (event: any) => onDragStart(event, map))
     .on('pointerup', () => onDragEnd(map))
     .on('pointerupoutside', () => onDragEnd(map))
     .on('pointermove', () => onDragMove(map));
 
-  // For mouse-only events
-  // .on('mousedown', onDragStart)
-  // .on('mouseup', onDragEnd)
-  // .on('mouseupoutside', onDragEnd)
-  // .on('mousemove', onDragMove);
-
-  // For touch-only events
-  // .on('touchstart', onDragStart)
-  // .on('touchend', onDragEnd)
-  // .on('touchendoutside', onDragEnd)
-  // .on('touchmove', onDragMove);
-
-  // move the sprite to its designated position
   map.x = 0;
   map.y = 0;
 
-  // add it to the stage
   playArea.stage.addChild(map);
 }
 
 function onDragStart(event: any, map: any) {
-  // store a reference to the data
-  // the reason for this is because of multitouch
-  // we want to track the movement of this particular touch
   map.data = event.data;
-  map.alpha = 0.5;
   map.dragging = true;
 }
 
 function onDragEnd(map: any) {
   map.alpha = 1;
   map.dragging = false;
-  // set the interaction data to null
   map.data = null;
 }
 
