0b901ac890bcbb4a807510c477539f03da5a41e0
justin wash <justin.wash@pm.me>
2021-03-07 19:57:40 -0600

    add google sign-in button for testing

--- client/src/components/interface/OuterSidebar.tsx
+++ client/src/components/interface/OuterSidebar.tsx
@@ -1,5 +1,17 @@
-const OuterSidebar = (props: { width: number }) => (
-  <div
+import React from 'react';
+
+const OuterSidebar = (props: { width: number }) => {
+  
+  function onSignIn(googleUser: any) {
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+  }
+  
+
+return <div
     id='placeholder-outer-sidebar'
     style={{
       background: '#222',
@@ -10,8 +22,10 @@ const OuterSidebar = (props: { width: number }) => (
       textAlign: 'center',
     }}
   >
+    <div className="g-signin2" data-onsuccess={onSignIn}></div>
+
     <img style={{ width: '80%' }} alt='user-icon' src='assets/placeholders/icons/user.png' />
   </div>
-);
+}
 
 export default OuterSidebar;
