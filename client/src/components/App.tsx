1f2e699b4c8929a5feb6b27d1ac6b66f75c53767
Justin Wash <justin.wash@pm.me>
2021-03-17 16:05:18 -0500

    move play page into component

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,40 +1,12 @@
-import React, { createRef, useEffect } from 'react';
-import playArea from './pixi/playArea';
-
-import useWindowDimensions from '../utilities/window';
-import OuterSidebar from './interface/OuterSidebar';
-import InnerSidebar from './interface/InnerSidebar';
-import Toolbox from './interface/Toolbox';
+import React from 'react';
 
 import AuthProvider from './contexts/AuthProvider';
+import PlayPage from './pages/PlayPage';
 
 const App = () => {
-  const canvasRef = createRef<HTMLDivElement>();
-
-  useEffect(() => {
-    canvasRef?.current?.appendChild(playArea.view);
-  }, [canvasRef]);
-
-  const { width, height } = useWindowDimensions();
-  const outerSidebarWidth = 80;
-  const innerSidebarWidth = 400;
-
-  useEffect(() => {
-    if (playArea && canvasRef.current) {
-      playArea.renderer.resize(width - outerSidebarWidth - innerSidebarWidth, height);
-    }
-  }, [canvasRef, width, height]);
-
   return (
     <AuthProvider>
-      <div style={{ width: '100vw' }} className='App'>
-        <OuterSidebar width={outerSidebarWidth} />
-        <InnerSidebar width={innerSidebarWidth} />
-        <Toolbox sidebarWidth={innerSidebarWidth + outerSidebarWidth} />
-
-        {/* render play area in this div. figure out how to get this into a component */}
-        <div style={{ overflow: 'auto' }} ref={canvasRef} />
-      </div>
+      <PlayPage />
     </AuthProvider>
   );
 };
