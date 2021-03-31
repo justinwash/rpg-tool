01f8df7ab12ef251d8f7699cf52d68136fd880ee
justin wash <justin.wash@pm.me>
2021-03-31 11:33:31 -0500

    POST not PUT

--- server/src/server/endpoint/session.rs
+++ server/src/server/endpoint/session.rs
@@ -5,17 +5,16 @@ use warp::Filter;
 
 pub fn create_session() -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone
 {
-  warp::put()
+  warp::post()
     .and(warp::path("session"))
     .and(warp::path::end())
     .and(create_session_request())
     .map(|session_request: CreateSessionRequest| {
       let db = get_db_connection();
       let new_session = crate::db::session::create_session(&db, session_request.user_id as i32);
-      println!("got create_session. Result: {:?}", new_session);
       warp::reply::json(&new_session.unwrap())
     })
-    .with(warp::cors().allow_any_origin().allow_methods(vec!["PUT"]))
+    .with(warp::cors().allow_any_origin().allow_methods(vec!["POST"]))
 }
 
 pub fn get_user_sessions(
@@ -27,7 +26,6 @@ pub fn get_user_sessions(
       let db = get_db_connection();
       let user_sessions =
         crate::db::session::get_user_sessions(&db, get_user_sessions_request.user_id as i32);
-      println!("got get_user_sessions. Result: {:?}", user_sessions);
       warp::reply::json(&user_sessions.unwrap())
     })
     .with(warp::cors().allow_any_origin().allow_methods(vec!["GET"]))
@@ -38,7 +36,6 @@ pub fn get_session() -> impl Filter<Extract = impl warp::Reply, Error = warp::Re
     .map(|session_uuid| {
       let db = get_db_connection();
       let session = crate::db::session::get_session(&db, session_uuid);
-      println!("got get_session. Result: {:?}", session);
       warp::reply::json(&session.unwrap())
     })
     .with(warp::cors().allow_any_origin().allow_methods(vec!["GET"]))
